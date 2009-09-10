0b8d23c6b2d064aa2376c6970eb6c1edab186498
swdyh <youhei@gmail.com>
2009-09-10 16:43:37 +0900

    version 0.0.14

--- lib/gisty.rb
+++ lib/gisty.rb
@@ -6,7 +6,7 @@ require 'rubygems'
 require 'nokogiri'
 
 class Gisty
-  VERSION   = '0.0.13'
+  VERSION   = '0.0.14'
   GIST_URL  = 'http://gist.github.com/'
   GISTY_URL = 'http://github.com/swdyh/gisty/tree/master'
 
