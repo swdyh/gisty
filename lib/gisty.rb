82401d6d78748b39047ff37ad5884fab53b5ae75
swdyh <youhei@gmail.com>
2011-03-01 21:17:56 +0900

    version 0.0.19

--- lib/gisty.rb
+++ lib/gisty.rb
@@ -7,7 +7,7 @@ require 'nokogiri'
 require 'net/https'
 
 class Gisty
-  VERSION   = '0.0.18'
+  VERSION   = '0.0.19'
   GIST_URL  = 'https://gist.github.com/'
   GISTY_URL = 'http://github.com/swdyh/gisty/tree/master'
 
