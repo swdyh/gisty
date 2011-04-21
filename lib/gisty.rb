37dee0d48a94726edddf2a3090ce939db6f80869
swdyh <youhei@gmail.com>
2011-04-21 13:17:10 +0900

    version 0.0.21

--- lib/gisty.rb
+++ lib/gisty.rb
@@ -7,7 +7,7 @@ require 'nokogiri'
 require 'net/https'
 
 class Gisty
-  VERSION   = '0.0.20'
+  VERSION   = '0.0.21'
   GIST_URL  = 'https://gist.github.com/'
   GISTY_URL = 'http://github.com/swdyh/gisty/tree/master'
   COMMAND_PATH = Pathname.new(File.join(File.dirname(__FILE__), 'commands')).realpath.to_s
